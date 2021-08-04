0339b9dc23aef8d4e22118379cdf08a7d5031b2b
Yukijudaii <820359253@qq.com>
2021-08-04 23:30:17 +0800

    第一版 boot sector
    第一版引导扇区，在已有代码的基础上增加补充了自己的信息。boot sector部分已完成。

---
+++ boot.asm
@@ -0,0 +1,27 @@
+;%define _BOOT_DEBUG_		; 制作 Boot Sector 时一定将此行注释掉!
+				; 去掉此行注释后可做成.COM文件易于调试:
+				;   nasm Boot.asm -o Boot.com 
+
+%ifdef  _BOOT_DEBUG_
+	org  0100h		
+%else
+	org  07c00h		
+%endif
+
+	mov	ax, cs
+	mov	ds, ax
+	mov	es, ax
+	call	DispStr		
+	jmp	$		
+DispStr:
+	mov	ax, BootMessage
+	mov	bp, ax		
+	mov	cx, 16		
+	mov	ax, 01301h	
+	mov	bx, 000ch	
+	mov	dl, 0
+	int	10h		
+	ret
+BootMessage:		db	"Welcome to Use my operate system"
+times 	510-($-$$)	db	0
+dw 	0xaa55			  
